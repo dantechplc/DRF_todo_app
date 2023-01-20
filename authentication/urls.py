88a8bf34a5c36e2d7efb8001fc93d09beb9114b9
Dantech <chukwujidan@gmail.com>
2023-01-20 02:51:32 +0100

    worked on Register view and serializers

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+from _ast import pattern
+from authentication.views import  RegisterAPIView
+from django.urls import path
+
+
+urlpatterns = [
+    path('register', RegisterAPIView.as_view(), name='register'),
+]
