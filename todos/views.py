8bf378d82c49afd3ee198dd66d70d15eae3fda75
Dantech <chukwujidan@gmail.com>
2023-01-22 09:59:34 +0100

    implemented django filter in TodoApiView

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,6 +13,12 @@ from todos.serializers import TodoSerializer
 class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['id', 'title', 'desc', 'is_complete']
+    search_fields = ['id', 'title', 'desc', 'is_complete']
+    ordering_fields = ['id', 'title', 'desc', 'is_complete']
+
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
@@ -26,5 +34,3 @@ class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
-
-
